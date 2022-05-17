b3d67f316ecf8d24b134df33cfd3ea164d2d3858
lxw617 <lixiawei521@qq.com>
2022-05-17 15:59:22 +0800

    feat:test3

--- main.go
+++ main.go
@@ -30,12 +30,7 @@ func init() {
 	//util.Setup()
 }
 func TestGit() {
-	fmt.Println("测试git")
-	fmt.Println("测试git")
-	fmt.Println("测试git")
-	fmt.Println("测试git")
-	fmt.Println("测试git")
-	fmt.Println("测试git")
+	fmt.Println("do you understand?")
 }
 func main() {
 
